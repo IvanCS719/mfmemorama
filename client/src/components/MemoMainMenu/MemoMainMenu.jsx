d9b994dabded1159070973fe1e9940d2f41f582b
IvanCS719 <ivancupil17@gmail.com>
2023-07-19 10:44:26 -0600

    Desarrollando los otros Layouts

--- client/src/components/MemoMainMenu/MemoMainMenu.jsx
+++ client/src/components/MemoMainMenu/MemoMainMenu.jsx
@@ -5,15 +5,16 @@ function MemoMainMenu() {
 
   return (
     //Se pasan la props a tablero
-    <main className='w-full min-h-screen flex items-center justify-center flex-col p-2'>
+    <main className='w-full min-h-screen flex items-center justify-between flex-col p-2 font-fontGeneral'>
       <div>
-        <p className='font-bold font-fontGeneral text-4xl'>MEMORAMA</p>
+        <p className='font-bold text-5xl text-center mb-2'>MEMORAMA</p>
+        <img src="logoMainMenu.png" alt="" className='w-96' />
       </div>
-      <div className='flex flex-col gap-4 text-2xl font-semibold font-fontGeneral'>
-        <button className='bg-blue-500 text-white w-60 py-3 rounded-3xl flex justify-center'>1 JUGADOR</button>
-        <button className='bg-blue-500 text-white w-60 py-3 rounded-3xl flex justify-center'>2 JUGADORES</button>
+      <div className='flex flex-col gap-4 text-2xl font-semibold'>
+        <button className='bg-blue-500 text-white w-60 py-3 rounded-2xl flex justify-center'>1 JUGADOR</button>
+        <button className='bg-blue-500 text-white w-60 py-3 rounded-2xl flex justify-center'>2 JUGADORES</button>
       </div>
-      <div className='py-4'>
+      <div className='font-semibold text-gray-800'>
         <a href='https://mercadofacil.mx/' target='_blank'>MercadoFácil.mx</a>
       </div>
     </main>
