dd29ab0e040138f6df27d2376e6d38b40fd3079c
IvanCS719 <ivancupil17@gmail.com>
2023-07-23 14:10:28 -0600

    Arreglando bugs

--- client/src/components/MemoAlert/MemoMessageTurno.jsx
+++ client/src/components/MemoAlert/MemoMessageTurno.jsx
@@ -1,14 +1,14 @@
 function MemoMessageTurno({mostrarMensajesTurno, turnoPlayer1Mess, turnoPlayer2Mess }) {
 
     return (
-        <div className={`fixed z-30 w-full flex items-center justify-center font-fontGeneral text-center font-bold ${mostrarMensajesTurno ? 'opacity-100 scale-100' : 'opacity-0 scale-0 pointer-events-none'}`}>
+        <div className={`fixed z-30 w-full h-full max-h-full flex items-center justify-center font-fontGeneral text-center font-bold ${mostrarMensajesTurno ? 'opacity-100 scale-100' : 'opacity-0 scale-0 pointer-events-none'}`}>
 
             <div className={`fixed inset-0 min-w-max flex items-center justify-center transition-all duration-200 ${turnoPlayer1Mess ? 'opacity-100 scale-100' : 'opacity-0 scale-0 pointer-events-none'}`} >
-                <p className='text-7xl'>Turno de<br />Rojo</p>
+                <p className='bg-red-600 p-3 rounded-2xl text-white text-7xl'>Turno de<br />Rojo</p>
             </div>
 
             <div className={`fixed inset-0 min-w-max flex items-center justify-center transition-all duration-200 ${turnoPlayer2Mess ? 'opacity-100 scale-100' : 'opacity-0 scale-0 pointer-events-none'}`} >
-                <p className='text-7xl'>Turno de<br />Azul</p>
+                <p className='bg-blue-600 p-3 rounded-2xl text-white text-7xl'>Turno de<br />Azul</p>
             </div>
 
         </div>
